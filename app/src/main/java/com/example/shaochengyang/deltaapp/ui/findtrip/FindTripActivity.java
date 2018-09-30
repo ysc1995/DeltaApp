a3db8b6821094bcb39529cc6870f0140840a92b4
ysc1995 <39675591+ysc1995@users.noreply.github.com>
2018-09-29 23:29:31 -0400

    find trip with bottom menu

--- app/src/main/java/com/example/shaochengyang/deltaapp/ui/findtrip/FindTripActivity.java
+++ app/src/main/java/com/example/shaochengyang/deltaapp/ui/findtrip/FindTripActivity.java
@@ -11,6 +11,8 @@ public class FindTripActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_trip);
+        
+
 
     }
 }
