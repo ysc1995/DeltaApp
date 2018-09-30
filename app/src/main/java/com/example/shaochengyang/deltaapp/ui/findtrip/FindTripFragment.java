758ff8d5ac429ee1f9f9e293b50ac228a5f3f90d
mikkiGuo <mikkiguo29@yahoo.com>
2018-09-30 17:07:58 -0500

    implement tab layout

---
+++ app/src/main/java/com/example/shaochengyang/deltaapp/ui/findtrip/FindTripFragment.java
@@ -0,0 +1,20 @@
+package com.example.shaochengyang.deltaapp.ui.findtrip;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.shaochengyang.deltaapp.R;
+
+public class FindTripFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frag_find_trip, container,false);
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/shaochengyang/deltaapp/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/shaochengyang/deltaapp/ui/login/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.shaochengyang.deltaapp.ui;
+package com.example.shaochengyang.deltaapp.ui.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
